ab12b9d0931a854440da013a35144594f4b1ba48
KeyserSozayme <Keithkng73@gmail.com>
2016-11-28 18:45:52 -0700

    Changed Directory Structure

--- ScriptingLab01 - Q1.py
+++
@@ -1,9 +0,0 @@
-radius = 8
-pi = 3.14159
-area = pi * radius ** 2
-
-print ("The area of a circle with a radius of " +
-       str(radius) +
-       " is " +
-       str(area))
-           
--- ScriptingLab01 - Q3.py
+++
@@ -1,8 +0,0 @@
-earthRadiusKM = 6371
-pi = 3.14159
-
-# Calculate the Earth's volume in cubic kilometers.
-earthVolume = (4/3 * pi * (earthRadiusKM ** 3))
-
-# Print the calculated volume.
-print("The volume of the earth is: "+ str(earthVolume))
