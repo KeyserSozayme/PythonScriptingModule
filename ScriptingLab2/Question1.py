315046ac396d020dffa4f6729f1acc0f35d32da1
Keith King <Keithkng73@gmail.com>
2016-12-05 21:27:30 -0700

    Added crappy program that prints primes under 100

---
+++ ScriptingLab2/Question1.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python
+
+Counter = 2
+
+while (Counter < 100):
+    
+    # We will assume the 'Counter' is prime and our program will try to disprove that assumption.
+    varIsCounterPrime = True
+    
+    # Set the variable 'i' to every number between 3 and the value of 'Counter' (But never equals 'Counter'),
+    # Then divide 'Counter' by 'i' and if the remainder is ever 0 then it is not prime.
+    for i in range (2, Counter):
+    	if (Counter % i == 0): varIsCounterPrime = False
+    
+    # If 'Counter' stayed prime print the value of 'Counter'
+    if (varIsCounterPrime): print(Counter)
+
+    # Increment 'Counter' by 1 and start again.
+    Counter += 1
