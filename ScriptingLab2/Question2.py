42dbd7b34fef74ebeadd1907b7fa4cde4bb8b613
Keith King <Keithkng73@gmail.com>
2016-12-05 21:43:10 -0700

    Added program that asks user to add integers together. When total is greater than 1000 exit program.

---
+++ ScriptingLab2/Question2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# Set our 'Default' running total.
+varRunningTotal = 0
+
+# As long as our total is less than, or equal to 1000, keep prompting us to add to that total.
+while (varRunningTotal <= 1000):
+    # Print current total and ask us to add to it.
+    varRunningTotal += int(input("The current total is " + str(varRunningTotal) + ", enter an integer to add to that total.: "))
+    
+# The while loop has been broken, this just tells us what the total was when we left the loop.
+print("\nThe Total was " + str(varRunningTotal) + ",\nExiting program...\n")
