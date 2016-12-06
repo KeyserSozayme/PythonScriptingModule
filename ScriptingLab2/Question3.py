0e38dfcee361e10387c958ebf7fcada8ebf0f613
Keith King <Keithkng73@gmail.com>
2016-12-06 14:29:33 -0700

    Changed code to correctly answer question in lab.

--- ScriptingLab2/Question3.py
+++ ScriptingLab2/Question3.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
-# Import the 'randrange' function fromm the 'random' library
-from random import randrange
-
 
 # Make a string and set it to somebody's input.
 strSentence = input("Write a beautiful passage you'd like to ruin.: ")
 
 # Define the vowels in the english alphabet. ('Y' doesn't count)
 listVowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+listReplaceVowels = ['O', 'o', 'i', 'a', 'i', 'e', 'a', 'e']
+indexReplaceVowels = 0
 
 print("You asked for this.\n")
 
 # Cycle throught the inputted string and set 'i' as each character.
 for i in strSentence:
-    # If 'i' is in the list of vowels, set it as a random vowel from the list of vowels.
-    # Then print i with the end character an empty string.
+    # If i is in the vowels list, set it to the character at index 0 in the replace vowels list.
     if (i in listVowels):
-        i = listVowels[randrange(0, 9)]
+        i = listReplaceVowels[indexReplaceVowels]
+        # Increment the value that we index our 'listReplaceVowels' at by 1, if it is over 7 (The last value in the replacelist) set is back to 0
+        indexReplaceVowels += 1
+        if (indexReplaceVowels > 7): indexReplaceVowels = 0
+    # Print i without being followed by a newline.
     print(i, end="")
 
 # Print a newline for more readability
