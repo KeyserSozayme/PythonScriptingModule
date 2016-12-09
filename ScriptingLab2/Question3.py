e8a68c674eccf899d890dee528219a0d9317bcfa
Keith King <Keithkng73@gmail.com>
2016-12-09 09:27:36 -0700

    Changed Question3 to properly replace vowels, it eplaces each vowel with the next vowel in the sequence (AEIOU) and keeps respect to upper or lower case.

--- ScriptingLab2/Question3.py
+++ ScriptingLab2/Question3.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python
 
+###
+#
+# This script asks the user for a string, then for each vowel in the string
+# it replaces that vowel with the next vowel in the sequence (A -> E, i -> o, u -> a).
+#
+###
+
+
 # Make a string and set it to somebody's input.
 strSentence = input("Write a beautiful passage you'd like to ruin.: ")
 
 # Define the vowels in the english alphabet. ('Y' doesn't count)
-listVowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-listReplaceVowels = ['O', 'o', 'i', 'a', 'i', 'e', 'a', 'e']
-indexReplaceVowels = 0
+listUpperVowels = ['A', 'E', 'I', 'O', 'U']
+listLowerVowels = ['a', 'e', 'i', 'o', 'u']
+indexUpper = 0
+indexLower = 0
 
 print("You asked for this.\n")
 
 # Cycle throught the inputted string and set 'i' as each character.
 for i in strSentence:
-    # If i is in the vowels list, set it to the character at index 0 in the replace vowels list.
-    if (i in listVowels):
-        i = listReplaceVowels[indexReplaceVowels]
-        # Increment the value that we index our 'listReplaceVowels' at by 1, if it is over 7 (The last value in the replacelist) set is back to 0
-        indexReplaceVowels += 1
-        if (indexReplaceVowels > 7): indexReplaceVowels = 0
-    # Print i without being followed by a newline.
+    # If i is in the list of uppercase vowels
+    if st(i in listUpperVowels):
+        # Set indexUpper to the index of (i + 1) in the list of uppercase vowels
+        indexUpper = (listUpperVowels.index(i) + 1)
+        # If i is an index higher than the length of the list, set it to 0
+        if (indexUpper > 4): indexUpper = 0
+        # Set i to the vowel at indexUpper in our list
+        i = listUpperVowels[indexUpper]
+
+    # Do the same as above but for lowercase vowels
+    if (i in listLowerVowels):
+        indexLower = (listLowerVowels.index(i) + 1)
+        if (indexLower > 4): indexLower = 0
+        i = listLowerVowels[indexLower]
+
+
     print(i, end="")
 
 # Print a newline for more readability
